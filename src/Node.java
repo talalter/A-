02fb5d16cd5bfc3bed1032fb5d4d40c10b6fc3fb
talalter <talalter95900@gmail.com>
2021-06-20 23:59:43 +0300

    Initial

---
+++ src/Node.java
@@ -0,0 +1,120 @@
+// Tal alter 206292450
+// Or abitbol 312614605
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Node {
+
+    char [] state;
+    Node parent;
+    int g;
+    int h;
+    int f;
+    String action = "";
+    public Node(char [] state,Node parent,int g,String action){
+        this.state=state;
+        this.parent=parent;
+        this.g=g;
+        this.h=calculate_h(state);
+        this.f = this.g+this.h;
+        this.action=action;
+
+    }
+    public boolean ifSolution() {
+        boolean opt1 = (this.state[0]=='W' && this.state[1]=='W' && this.state[2]=='W');
+        if (opt1) {
+            return true;
+        }
+        for (int i=0;i<4;i++){
+            if(state[i]=='B'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int calculate_O_index() {
+        for (int i = 0; i < this.state.length; i++) {
+            if (this.state[i] == 'O') {
+                return i;
+            }
+        }
+        System.out.println("mistake");
+        return -1;
+    }
+    //action is a number indicates : 1-switch one index from laft, 2-switch 2 indexes from laft, 3- switch 1 from right, 4-.
+    public Node childNode(int action, int index) {
+        char [] state = this.state.clone();
+        String action_string;
+        action_string="";
+        int new_g=this.g;
+        if(action==0){
+            state[index]=state[index+1];
+            state[index+1]='O';
+            new_g=1+this.g;
+            action_string = "switching "+state[index]+" in index "+(index+1)+" and blank in index "+index+"\n   Cost of this action is: "+ 1;
+        }
+        if(action==1){
+            state[index]=state[index-1];
+            state[index-1]='O';
+            new_g=1+this.g;
+            action_string = "switching "+state[index]+" in index "+(index-1)+" and blank in index "+index+"\n   Cost of this action is: "+ 1;
+
+        }
+        if(action==2){
+            state[index]=state[index+2];
+            state[index+2]='O';
+            new_g=2+this.g;
+            action_string = "switching "+state[index]+" in index "+(index+2)+" and blank in index "+index+"\n   Cost of this action is: "+ 2;
+
+        }
+        if(action==3){
+            state[index]=state[index-2];
+            state[index-2]='O';
+            new_g=2+this.g;
+            action_string = "switching "+state[index]+" in index "+(index-2)+" and blank in index "+index+"\n   Cost of this action is: "+ 2;
+
+        }
+        Node child = new Node(state,this,new_g,action_string);
+        return child;
+    }
+
+    public int calculate_h(char [] state){
+        int count=0;
+        for(int i=state.length-1;i>0;i--){
+            if(state[i]=='W'){
+                for(int j=i-1;j>0;j--){
+                    if(state[j]=='B'){
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main (String [] args){
+        String str = "BOBWWBW";
+        char[] c = str.toCharArray();
+//        Node n1 = new Node(charArray,null,0,"");
+        PriorityQueue<Node> frontier = new PriorityQueue<Node>(new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                return Integer.compare(node1.g, node2.g);
+            }
+        });
+        Node n1 = new Node(c,null,1,"11");
+        Node n2 = new Node(c,null,3,"33");
+        Node n3 = new Node(c,null,2,"22");
+        Node n4 = new Node(c,null,4,"44");
+        frontier.add(n1);
+        frontier.add(n2);
+        frontier.add(n3);
+        frontier.add(n4);
+        while(!frontier.isEmpty()){
+            System.out.println(frontier.poll().action);
+        }
+
+    }
+}
---
+++ src/Problem.java
@@ -0,0 +1,187 @@
+// Tal alter 206292450
+// Or abitbol 312614605
+
+import java.util.*;
+public class Problem extends Object {
+    private PriorityQueue<Node> frontier;
+    private Set<Node> explorer_set;
+
+
+    public Problem() {
+//        char[] charArray = input.toCharArray();
+        this.frontier = new PriorityQueue<Node>(new Comparator<Node>() {
+            @Override
+            public int compare(Node node1, Node node2) {
+                return Integer.compare(node1.f, node2.f);
+            }
+        });
+        this.explorer_set = new HashSet<>();
+    }
+
+    public LinkedList<Node> get_path(Node target) {
+        LinkedList<Node> path = new LinkedList<Node>();
+        while (target != null) {
+            path.add(target);
+            target = target.parent;
+        }
+        return path;
+    }
+
+    public LinkedList<Node> reverse_path(LinkedList<Node> path) {
+        LinkedList<Node> reversed = new LinkedList<Node>();
+        for (int i = path.size() - 1; i >= 0; i--)
+            reversed.add(path.get(i));
+        return reversed;
+    }
+
+    public void printPath(LinkedList<Node> path) {
+        System.out.print("0. ");
+        System.out.println(path.get(0).state);
+        for (int i = 1; i < path.size(); i++) {
+            System.out.println("   " + path.get(i).action);
+            System.out.println("   Total cost so far is: " + path.get(i).g);
+            if (i != path.size() - 1) {
+                System.out.print(i + ". ");
+                System.out.println(path.get(i).state);
+            } else {
+                System.out.print("Solution found: ");
+                System.out.print(path.get(i).state);
+                System.out.println(", lowest cost possible is: " + path.get(i).g);
+            }
+
+        }
+    }
+
+    public Node insideQueue(Node node, PriorityQueue<Node> q) {
+        for (Node ele : q) {
+            if (Arrays.equals(ele.state, node.state)) {
+                return ele;
+            }
+        }
+        return null;
+    }
+
+    public boolean insideSet(Node node, Set<Node> h) {
+        for (Node ele : h) {
+            if (Arrays.equals(ele.state, node.state))
+                return true;
+        }
+        return false;
+    }
+
+
+    public boolean search(Node start) {
+        HashMap<Node, Node> path = new HashMap<Node, Node>();
+        path.put(start, null);
+        this.frontier.add(start);
+        path.put(start, start);
+        while (true) {
+            if (frontier.isEmpty()) {
+                System.out.println("failure");
+                return false;
+            }
+            Node node = frontier.poll();
+            ;
+            if (node.ifSolution()) {
+                printPath(reverse_path(get_path(node)));
+                return true;
+            }
+            explorer_set.add(node);
+            boolean[] actions = check_valid(node);
+            for (int i = 0; i < actions.length; i++) {
+                if (actions[i]) {
+                    Node child = node.childNode(i, node.calculate_O_index());
+                    Node is_inside_frontier = insideQueue(child, this.frontier);
+                    boolean is_inside_explorer_set = insideSet(child, this.explorer_set);
+                    if (!is_inside_explorer_set || is_inside_frontier == null) {
+                        this.frontier.add(child);
+                    } else {
+                        if (is_inside_frontier != null) {
+                            if (is_inside_frontier.f < child.f) {
+                                this.frontier.remove(is_inside_frontier);
+                                this.frontier.add(child);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private boolean[] check_valid(Node node) {
+        int index = node.calculate_O_index();
+        boolean[] arr = new boolean[]{true, true, true, true};
+        switch (index) {
+            case 0:
+                arr[1] = false;
+                arr[3] = false;
+                break;
+            case 1:
+                arr[3] = false;
+                break;
+            case 5:
+                arr[2] = false;
+                break;
+            case 6:
+                arr[0] = false;
+                arr[2] = false;
+
+        }
+//        if (index == 0) {
+//            arr[1] = false;
+//            arr[3] = false;
+//        }
+//        if (index == 1) {
+//            arr[3] = false;
+//        }
+//        if (index == 5) {
+//            arr[2] = false;
+//        }
+//        if (index == 6) {
+//            arr[0] = false;
+//            arr[2] = false;
+//        }
+        return arr;
+    }
+
+    public boolean checkValidString(String str) {
+        char[] input = str.toCharArray();
+        short B = 0;
+        short O = 0;
+        short W = 0;
+        if (input.length != 7)
+            return false;
+        for (int i = 0; i < 7; i++) {
+            if (input[i] == 'W')
+                W++;
+            else if (input[i] == 'B')
+                B++;
+            else
+                O++;
+        }
+        if (W == 3 && B == 3 && O == 1)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem p = new Problem();
+        Scanner sc = new Scanner(System.in);
+        boolean play = true;
+        while (play) {
+            System.out.print("Enter a string, or press 'q' if you want to quit");
+            String str = sc.nextLine();
+            if(str.equals("q")){
+                System.out.println("END");
+                break;
+            }
+            System.out.println(str);
+            if (p.checkValidString(str))
+                p.search(new Node(str.toCharArray(), null, 0, ""));
+            else
+                System.out.println("NOT VALID STRING");
+
+        }
+    }
+}
